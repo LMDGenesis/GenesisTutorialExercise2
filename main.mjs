848f9a9f925be90d93e64b55c970f3314f428e5c
LMDGenesis <ldiliberto@genesistrading.com>
2022-03-29 08:39:12 -0400

    Mnemonic
    Working on it

--- main.mjs
+++ main.mjs
@@ -1,20 +1,34 @@
 import bip39 from 'bip39'
+import bitcoinjs from 'bitcoinjs-lib'
+import { ECPairFactory } from 'ecpair';
+import * as ecc from 'tiny-secp256k1';
 
 const mnemonic = bip39.generateMnemonic(256)
 //Generates random mnemonic
 //console.log(mnemonic)
 //Overiting for conistatncy
 const mnemonicSaved = "clip finish garbage off nice bicycle memory mouse shy multiply bonus busy client tattoo hamster gold slam lava orange pave arm grocery midnight name"
-console.log(mnemonicSaved)
+console.log(mnemonicSaved)//Prints Mnemon
 
 const b = bip39.mnemonicToSeedSync('basket')
 //Taking a mnemonic phrase to a list of 64 numbers that stay consistantof the phrase you enter
-console.log(b)
+//console.log(b)
 
 const mTOe = bip39.mnemonicToEntropy(mnemonicSaved)
-//Takes mnemonic to entropy which is a private key
-console.log(mTOe)
+//Takes mnemonic to entropy which will be used to make the private key
+console.log(mTOe)//Prints String
 
 const eTOm = bip39.entropyToMnemonic(mTOe)
-//Takes mnemonic to entropy which is a private key
-console.log(eTOm)
+//Goes other way
+console.log(eTOm)//Prints Mnemon
+
+//Generating a TestNet Key Pair
+//const ECPair = ECPairFactory(ecc);
+const TESTNET = bitcoinjs.networks.testnet;
+
+const ECPair = ECPairFactory(ecc);
+
+const keyPair = ECPair.fromWIF(
+    '2b4ae17e4ca9562be2bc85c7922c658f92adbc9a2b22cb0fba6f50c0bacda31c',
+  );
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
